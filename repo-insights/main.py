c23fbeb655a8e6e171b573630a3b7063548d4083
yuizho <yui-ito@bm-sms.co.jp>
2021-06-03 22:05:34 +0900

    introduce click as command line argument parser instead of fire

--- repo-insights/main.py
+++ repo-insights/main.py
@@ -1,16 +1,35 @@
-import fire
+import click
 from lead_time import fetch_lead_time_record, LeadTimeRecord
+from datetime import datetime, timedelta
+
+ONE_MONTH_BEFORE = (datetime.now() - timedelta(days=30)).strftime("%Y-%m-%d")
 
 
-# TODO: from_dateのデフォルト値は仮 (一ヶ月前とかがデフォルトだといいかもね)
-def lead_time(repo_name="", token="", from_date="2021-05-01", base="master"):
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.argument("repository_name")
+@click.argument("token")
+@click.option(
+    "--first-merged-date",
+    "-f",
+    default=ONE_MONTH_BEFORE,
+    show_default=True,
+    help="first merged date to filter PR data (format: yyyy-mm-dd)",
+)
+@click.option(
+    "--base", "-b", default="master", show_default=True, help="the base branch of PR"
+)
+def lead_time(repository_name, token, first_merged_date, base):
     """
-    this command will fetch relese frequency of specified repos
-    :param repo_name: the target repository name (e.g. yuizho/repo-insights)
-    :param token: your github token
-    :param base: the base branch of PR
+    This command allows you to calculate lead time of specified GitHub repository by PR activity.
+
+    Usage: repo-insights lead-time "yuizho/repo-insights" "<your personal token of GitHub>"
     """
-    records = fetch_lead_time_record(repo_name, token, from_date, base)
+    records = fetch_lead_time_record(repository_name, token, first_merged_date, base)
 
     # TODO: ラベルのフィルターするならここで実施
     print("\t".join(LeadTimeRecord.get_fields_name()))
@@ -18,5 +37,9 @@ def lead_time(repo_name="", token="", from_date="2021-05-01", base="master"):
         print(record)
 
 
+def main():
+    cli()
+
+
 if __name__ == "__main__":
-    fire.Fire()
+    main()
