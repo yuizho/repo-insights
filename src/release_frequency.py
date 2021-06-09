557ffbf97ff59e8cf7a823f77dd1b3e8f299b361
yuizho <yui-ito@bm-sms.co.jp>
2021-06-09 23:27:14 +0900

    modify release_frequency.py

--- src/release_frequency.py
+++ src/release_frequency.py
@@ -1,3 +1,4 @@
+from attr import dataclass
 from github_api import DATETIME_FORMAT
 from github_api import Client
 from gql import gql
@@ -5,24 +6,31 @@ from datetime import datetime, timedelta
 from yaspin import yaspin
 
 
-def create_release_records(json):
+def create_releases(json):
     result = []
-    prev_published_at = None
     for pr in json["repository"]["releases"]["edges"]:
         if pr["node"]["isDraft"]:
             continue
         title = pr["node"]["name"]
         url = pr["node"]["url"]
         published_at = datetime.strptime(pr["node"]["publishedAt"], DATETIME_FORMAT)
-        frequency = published_at - prev_published_at if prev_published_at else timedelta()
-        result.append(ReleaseRecord(title, url, published_at, frequency))
-        prev_published_at = published_at
+        result.append(Release(title, url, published_at))
     return result
 
 
 def get_next_cursor(json):
     edges = json["repository"]["releases"]["edges"]
     return edges[0]["cursor"] if edges else None
+
+
+def create_releases_records(releases):
+    records = []
+    prev_published_at = None
+    for release in sorted(releases, key=lambda r: r.published_at):
+        frequency = release.published_at - prev_published_at if prev_published_at else timedelta()
+        records.append(ReleaseRecord(release.title, release.url, release.published_at, frequency))
+        prev_published_at = release.published_at
+    return records
 
 
 @yaspin(text="Fetching Release data...")
@@ -50,7 +58,7 @@ def fetch_release_records(repo_name, token, from_date, per_page=100):
     client = Client(token)
     owner, name = repo_name.split("/")
     cursor = None
-    records = []
+    releases = []
 
     while True:
         variables = {
@@ -61,18 +69,25 @@ def fetch_release_records(repo_name, token, from_date, per_page=100):
         }
 
         resp = client.execute(query, variables)
-        records_this_time = [
-            record
-            for record in create_release_records(resp)
-            if record.published_at > datetime.strptime(from_date, "%Y-%m-%d")
+        release_this_time = [
+            release
+            for release in create_releases(resp)
+            if release.published_at > datetime.strptime(from_date, "%Y-%m-%d")
         ]
-        records += records_this_time
-        if len(records_this_time) < per_page:
+        releases += release_this_time
+        if len(release_this_time) < per_page:
             break
 
         cursor = get_next_cursor(resp)
 
-    return records
+    return create_releases_records(releases)
+
+
+@dataclass
+class Release:
+    title: str
+    url: str
+    published_at: datetime
 
 
 class ReleaseRecord:
